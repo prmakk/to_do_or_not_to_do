f78e383b8a3d730e7de8e8d43c7218ac88e5f49c
prmk <priymakvlad420@gmail.com>
2023-09-25 17:34:59 +0300

    redesign + new logic

--- js/script.js
+++ js/script.js
@@ -6,25 +6,36 @@ let taskList = document.querySelector('.todo__list-ul');
 let clearBtn = document.querySelector('.clear');
 let task = document.querySelector('.todo__list-ul-item');
 
-showTask();
+window.onload = () => {
+    showTask();
+    deleteTask();
+    doneTask();
+}
 
 addBtn.onclick = () => {
     let displayMsg = '';
     if(taskInput.value == ""){
         alert('You must type something before adding...');
     }
+    else if (taskInput.value.length >= 16){
+        alert('Enter a task less than 16 characters');
+    }
     else{
+        location.reload();
         displayMsg += `
         <li class="todo__list-ul-item">
-            <label>${taskInput.value}</label>
             <input type="checkbox" class="checkboxInput">
+            <label>${taskInput.value}</label>
+            <i class="gg-trash"></i>
          </li>
         `
         taskList.innerHTML += displayMsg;
+        deleteTask();
     }
     taskInput.value = "";
     saveData();
 }
+
 
 clearBtn.onclick = () =>{
     localStorage.clear();
@@ -38,4 +49,23 @@ function showTask(){
     taskList.innerHTML = localStorage.getItem('data');
 }
 
- 
+function deleteTask(){
+    const deleteButtons = document.querySelectorAll('.gg-trash');
+
+    deleteButtons.forEach(button => {
+        button.onclick = () =>{
+            button.parentNode.remove();
+            saveData();
+        }
+    });
+}
+
+function doneTask(){
+    const allCheckboxes = document.querySelectorAll('.checkboxInput');
+    allCheckboxes.forEach(chk => {
+        chk.onchange = () =>{
+            chk.parentNode.remove();
+            saveData();
+        }
+    });
+}
